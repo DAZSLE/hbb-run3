d6c983b8cd15bac61cfba8852d3f4a91847ef388
Gabi Hamilton <gabihamilton@gmail.com>
2026-03-31 15:49:03 -0500

    split had and lep vgamma

--- data/v14_private.py
+++ data/v14_private.py
@@ -136,7 +136,13 @@ def get_datasets():  ## PRIVATE production with GloParTv2
                 # "DYto2L-4Jets_MLL-50_3J": "/store/group/lpcpfnano/PFNano_Run3/25v2/",
                 # "DYto2L-4Jets_MLL-50_4J": "/store/group/lpcpfnano/PFNano_Run3/25v2/",
             },
-            "VGamma": {
+            "VGammaHad": {
+                "ZGto2QG-1Jets_PTG-100to200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2QG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
+                "ZGto2QG-1Jets_PTG-200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2QG-1Jets_PTG-200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
+                "WGto2QG-1Jets_PTG-100to200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGto2QG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
+                "WGto2QG-1Jets_PTG-200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGto2QG-1Jets_PTG-200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
+            },
+            "VGammaLep": {
                 "WGtoLNuG-1Jets_PTG-100to200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGtoLNuG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
                 "WGtoLNuG-1Jets_PTG-200to400": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGtoLNuG-1Jets_PTG-200to400_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
                 "WGtoLNuG-1Jets_PTG-400to600": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGtoLNuG-1Jets_PTG-400to600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
@@ -145,10 +151,6 @@ def get_datasets():  ## PRIVATE production with GloParTv2
                 "ZGto2NuG-1Jets_PTG-100to200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2NuG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
                 "ZGto2NuG-1Jets_PTG-200to400": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2NuG-1Jets_PTG-200to400_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
                 "ZGto2NuG-1Jets_PTG-600": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2NuG-1Jets_PTG-600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
-                "ZGto2QG-1Jets_PTG-100to200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2QG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
-                "ZGto2QG-1Jets_PTG-200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/ZGto2QG-1Jets_PTG-200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
-                "WGto2QG-1Jets_PTG-100to200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGto2QG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
-                "WGto2QG-1Jets_PTG-200": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/WGto2QG-1Jets_PTG-200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8",
             },
             "EWKV": {
                 "VBFZto2Q": "/store/group/lpcpfnano/PFNano_Run3/25v2/roguljic/mc_2022/VBFZto2Q_TuneCP5_13p6TeV_madgraph-pythia8",
